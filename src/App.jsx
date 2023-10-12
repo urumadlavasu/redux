d573906123f51efe8eb7b933bb3077c0272deaa9
urumadlavasu <urumadlavasu04@gmail.com>
2023-10-12 16:57:26 +0530

    crud

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Link, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="mybox">
+      <h1>app</h1>
       <Link to='/countries'>Countries</Link>&nbsp;&nbsp;&nbsp;
       <Link to='/products'>Products</Link>&nbsp;&nbsp;&nbsp;
       <Link to='/counter'>Counter</Link>&nbsp;&nbsp;&nbsp;
