8e11301896efcd8c2942a59b0c4c1e54a953e4a4
urumadlavasu <urumadlavasu04@gmail.com>
2023-10-13 15:33:48 +0530

    reduxquerycrud

--- src/features/countries/CountryDetails.jsx
+++ src/features/countries/CountryDetails.jsx
@@ -4,12 +4,12 @@ import { useGetCountryDetailsByNameQuery } from '../../services/countries'
 
 function CountryDetails() {
    var {cname} = useParams()
-      var {data,isLoading}  = useGetCountryDetailsByNameQuery(cname)
+                var {data,isLoading}  = useGetCountryDetailsByNameQuery(cname)
   return (
     <div className='mybox'>
         <h1>CountryDetails of {cname}</h1>
         {
-          // console.log(data)
+        
           data && <img src={data[0].flags[0]} alt="" width="200px" />
         }
     </div>
